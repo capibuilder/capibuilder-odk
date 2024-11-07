f5bf81702cfda21b04b5b7d33f3ddbdd62b19001
CAPIBuilder <admin@capibuilder.org>
2024-11-08 00:49:37 +0530

    added repeat-group count

--- src/context/surveyStores.ts
+++ src/context/surveyStores.ts
@@ -1,1 +1,25 @@
- 
+import { create } from "zustand";
+
+interface SurveyState {
+  data: {
+    title: string;
+    fields: {
+      [key: string]: any;
+    };
+    entity?: any;
+    langs?: string[];
+    dataset?: string;
+    uniqueIdentifier?: string;
+  };
+  setData: (data: any) => void;
+  // ... any other state properties and methods
+}
+
+export const useSurveyStore = create<SurveyState>((set) => ({
+  data: {
+    title: '',
+    fields: {},
+  },
+  setData: (data) => set({ data }),
+  // ... other state methods
+})); 
