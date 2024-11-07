205895a7a72af308e48fce5b0ee398aeb0b53958
CAPIBuilder <admin@capibuilder.org>
2024-11-08 01:01:50 +0530

    fixed import

--- src/Containers/DesignSurvey/components/ContentHeader/index.tsx
+++ src/Containers/DesignSurvey/components/ContentHeader/index.tsx
@@ -1,6 +1,6 @@
 import { AddIcon, CloseIcon, MenuIcon, TickIcon } from "@/assets";
 import { DragDropWrapper } from "@/components";
-import useSurveyStore from "@/context/surveyStores";
+import { useSurveyStore } from "@/context/surveyStores";
 import { questionField } from "@/interfaces/questionFields";
 import { Fragment, useCallback, useEffect, useMemo, useState } from "react";
 import { shallow } from "zustand/shallow";
